c70ab36f3beaf96c58b8befe9b65895c397f0b64
Appvantage Bran <saobran.aung@appvantage.asia>
2023-07-11 14:53:49 +0630

    Code update for Auth Controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using API.DBContext;
+using API.Interface;
+using API.Model;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _iconfiguration;
+        private readonly IJWTManagerService _jWTManager;
+        private readonly IMapper _mapper;
+        public AuthController(ApplicationDbContext context, IConfiguration iconfiguration, IJWTManagerService jWTManager, IMapper mapper)
+        {
+            _context = context;
+            _iconfiguration = iconfiguration;
+            _mapper = mapper;
+            this._jWTManager = jWTManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Login")]
+        public async Task<User> Login(User data)
+        {
+            var response = new User();
+
+            try
+            {
+                var token = _jWTManager.AuthenticateTradenet2(response.data.htathaka_no);
+                response.Token = token.Token;
+                response.Permission = token.Permission;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                var error = ex;
+                return response;
+            }
+        }
+    }
+}
