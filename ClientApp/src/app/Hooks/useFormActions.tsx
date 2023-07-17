4f744f786caebce6f01ef44f2fde9fd115fc0875
Appvantage Bran <saobran.aung@appvantage.asia>
2023-07-17 16:30:17 +0630

    Code update for notifications

--- ClientApp/src/app/Hooks/useFormActions.tsx
+++ ClientApp/src/app/Hooks/useFormActions.tsx
@@ -1,26 +1,57 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Post, Put, Delete } from '../services/BasicHttpServices';
+import { Modal } from 'antd';
 
 const useFormActions = (id: string, action: string, APIURL: string) => {
   const [writeLoading, setWriteLoading] = useState(false);
+  const success = () => {
+    Modal.success({
+      title: 'Success',
+      content: 'Operation is successfully compleed...',
+    });
+  };
+
+  const error = () => {
+    Modal.error({
+      title: 'Error',
+      content: 'Something went wrong...',
+    });
+  };
 
   const onFinish = useCallback(
     (values: unknown) => {
       setWriteLoading(true);
       if (action === 'New') {
         Post(APIURL, values)
-          .then((x) => setWriteLoading(false))
-          .catch((err) => setWriteLoading(false));
+          .then((x) => {
+            setWriteLoading(false);
+            success();
+          })
+          .catch((err) => {
+            setWriteLoading(false);
+            error();
+          });
       } else if (action === 'Edit') {
         Put(APIURL, id, values)
-          .then((x) => setWriteLoading(false))
-          .catch((err) => setWriteLoading(false));
+          .then((x) => {
+            setWriteLoading(false);
+            success();
+          })
+          .catch((err) => {
+            setWriteLoading(false);
+            error();
+          });
       } else if (action === 'Delete') {
         Delete(APIURL, id)
-          .then((x) => setWriteLoading(false))
-          .catch((err) => setWriteLoading(false));
+          .then((x) => {
+            setWriteLoading(false);
+            success();
+          })
+          .catch((err) => {
+            setWriteLoading(false);
+            error();
+          });
       } else if (action === 'Detail') {
-        console.log();
         setWriteLoading(false);
       }
     },
