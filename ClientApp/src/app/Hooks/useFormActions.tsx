f0ba0959a4305934b142e3b7b4bb184394c90631
Appvantage Bran <saobran.aung@appvantage.asia>
2023-07-17 17:01:51 +0630

    Code update for unnecessary imports

--- ClientApp/src/app/Hooks/useFormActions.tsx
+++ ClientApp/src/app/Hooks/useFormActions.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Post, Put, Delete } from '../services/BasicHttpServices';
 import { Modal } from 'antd';
 
