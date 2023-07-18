5a45b1ffb130993832bc3d1b701a73096cf4f2e4
Appvantage Bran <saobran.aung@appvantage.asia>
2023-07-18 14:29:22 +0630

    Bug fixed

--- ClientApp/src/app/app.tsx
+++ ClientApp/src/app/app.tsx
@@ -1,23 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
-import AppContext, { UserPayload } from './components/Context/Context';
-import { useEffect, useState } from 'react';
-import { AnyObject } from 'antd/es/table/Table';
-import LayoutComponent from './components/Layout/LayoutComponent';
-import Login from './components/Login/Login';
-import axiosInstance from './services/AxiosInstance';
+import AppContext from './components/Context/Context';
 import useAuthCheck from './Hooks/useAuthCheck';
-type User = {
-  Name: string;
-  Password: string;
-  Permission: string;
-};
-
-type Token = {
-  token: string;
-  refreshToken: string;
-  userId: string;
-  permission: string;
-};
 
 export function App() {
   const { template, data, updateData } = useAuthCheck();
